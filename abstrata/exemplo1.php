0d77d4981256f213dcb0a393ac49306b5ff07378
William <william.teletabs@hotmail.com>
2023-02-28 11:38:33 -0300

    Exemplos de INTERFACE e CLASSE ABSTRATA

---
+++ abstrata/exemplo1.php
@@ -0,0 +1,32 @@
+<?php
+
+interface Veiculo{
+  public function acelerar($velocidade);
+  public function frear($velocidade);
+  public function trocarMarcha($marcha);
+
+}
+
+ abstract class Automovel implements Veiculo{
+  public function acelerar($velocidade){
+    echo "O carro acelerou até " . $velocidade . " km/h";
+  }
+
+  public function frear($velocidade){
+    echo "O veículo frenou até " . $velocidade . " km/h";
+  }
+
+  public function trocarMarcha($marcha){
+    echo "O veículo engatou a marcha " . $marcha;
+  }
+}
+
+  class Audi extends Automovel{
+    public function pintar(){
+
+    }
+  }
+
+  $carro = new Audi();
+  $carro -> acelerar(300);
+?>
